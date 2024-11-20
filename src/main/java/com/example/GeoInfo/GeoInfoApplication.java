d77584dc1004349f8e4983245891ab9d0a6d283c
thein3rovert <danielolaibi@gmail.com>
2024-11-20 15:46:54 +0000

    Feact Api Data test
    capital fetch successfully

--- src/main/java/com/example/GeoInfo/GeoInfoApplication.java
+++ src/main/java/com/example/GeoInfo/GeoInfoApplication.java
@@ -15,7 +15,20 @@ public class GeoInfoApplication {
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(GeoInfoApplication.class, args);
+		 //Http client for getting the request
+		HttpRequest getRequest = HttpRequest.newBuilder()
+				//.uri(new URI("https://countriesnow.space/api/v0.1/countries/capital"))
+				.uri(new URI("https://countriesnow.space/api/v0.1/countries/flag/images"))
+				.GET()
+				.build();
+		// Http client for sending the request
+		HttpClient client = HttpClient.newHttpClient();
+		HttpResponse<String> response = client.send(getRequest, HttpResponse.BodyHandlers.ofString());
 
+		//Print the response to the console
+		System.out.println(response.statusCode());
+		System.out.println(response.body());
+	}
 	}
 
 }
