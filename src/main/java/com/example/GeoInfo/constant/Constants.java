bfc3fd443b3c21e6e57286f831ae94d3b7d5ca9e
thein3rovert <danielolaibi@gmail.com>
2024-11-23 00:33:10 +0000

    Create a Constants class to store API endpoint URLs for images, cities, and capitals

---
+++ src/main/java/com/example/GeoInfo/constant/Constants.java
@@ -0,0 +1,7 @@
+package com.example.GeoInfo.constant;
+
+public class Constants {
+    public static final String IMAGE_API_URL = "https://countriesnow.space/api/v0.1/countries/flag/images";
+    public static final String CITIES_API_URL = "https://countriesnow.space/api/v0.1/countries/population/cities";
+    public static final String CAPITAL_API_URL = "https://countriesnow.space/api/v0.1/countries/capital";
+}
